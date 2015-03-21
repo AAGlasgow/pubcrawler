456a30c677d49fb9253d9cae4e50385fa74365a0
afleming <fleming.adamm@gmail.com>
2015-03-21 20:09:32 +0000

    pop script additions, Index list initial tweaks

--- populate_rango.py
+++ populate_rango.py
@@ -13,6 +13,7 @@ def populate():
     butch_user = add_user('Butch', 'butch@gmail.com', 'myPassword')
     peter_user = add_user('Peter', 'peter@gmail.com', 'myPassword')
     rain_user = add_user('Rain', 'rain@gmail.com', 'myPassword')
+    test_user = test_user('test', 'test@gmail.com', 'test')
 
     dram_pub = add_pub('DRAM!', 'ChIJKcWC6i1EiEgRrAN2HMyTv0U')
     nice_n_sleazy_pub = add_pub('Nice N Sleazy', 'ChIJ2U7jlChEiEgRaqr1lWpQ2RQ')
@@ -23,6 +24,11 @@ def populate():
 
     get_pished_crawl = add_crawl(butch_user, 'Get Pished', 'In alcohols defense Ive done some dumb shit while completely sober too!', False, "", False, "", [dram_pub, guu_pub, curlers_rest_pub])
     hippy_crawl_crawl = add_crawl(rain_user, 'Hippy Crawl', 'Best chances to come by pills youll have on the toilets', False, "", False, "", [king_tuts_pub, buff_club_pub, nice_n_sleazy_pub])
+    test_generic_1 = add_crawl(butch_user, 'test_1', '...', False, "", False, "", [dram_pub, guu_pub, curlers_rest_pub])
+    test_generic_2 = add_crawl(butch_user, 'test_2', '...', False, "", False, "", [dram_pub, guu_pub, curlers_rest_pub])
+    test_generic_3 = add_crawl(butch_user, 'test_3', '...', False, "", False, "", [dram_pub, guu_pub, curlers_rest_pub])
+    test_generic_4 = add_crawl(butch_user, 'test_4', '...', False, "", False, "", [dram_pub, guu_pub, curlers_rest_pub])
+    test_generic_5 = add_crawl(butch_user, 'test_5', '...', False, "", False, "", [dram_pub, guu_pub, curlers_rest_pub])
 
     hater_review = add_review(rain_user, get_pished_crawl, False, 'I didnt like it at all. There was no pub where anyone could sell me some weed or other drugs. Everyone else seemed to enjoy it though. Mainstream shite!')
     love_review = add_review(peter_user, get_pished_crawl, True, 'Was fucked that night, woke up the next day in my own sick somewhere in Dundee. Best night evaaa!')
@@ -44,80 +50,6 @@ def populate():
     for r in Review.objects.all():
         print r
 
-    
-
-##    python_cat = add_cat('Python', 128, 64)
-##
-##    add_page(cat=python_cat,
-##        title="Official Python Tutorial",
-##        url="http://docs.python.org/2/tutorial/",
-##        views=40)
-##
-##    add_page(cat=python_cat,
-##        title="How to Think like a Computer Scientist",
-##        url="http://www.greenteapress.com/thinkpython/",
-##        views=40)
-##
-##    add_page(cat=python_cat,
-##        title="Learn Python in 10 Minutes",
-##        url="http://www.korokithakis.net/tutorials/python/",
-##        views=48)
-##
-##    django_cat = add_cat("Django", 64, 32)
-##
-##    add_page(cat=django_cat,
-##        title="Official Django Tutorial",
-##        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
-##        views=20)
-##
-##    add_page(cat=django_cat,
-##        title="Django Rocks",
-##        url="http://www.djangorocks.com/",
-##        views=20)
-##
-##    add_page(cat=django_cat,
-##        title="How to Tango with Django",
-##        url="http://www.tangowithdjango.com/",
-##        views=24)
-##
-##    frame_cat = add_cat("Other Frameworks", 32, 16)
-##
-##    add_page(cat=frame_cat,
-##        title="Bottle",
-##        url="http://bottlepy.org/docs/dev/",
-##        views=16)
-##
-##    add_page(cat=frame_cat,
-##        title="Flask",
-##        url="http://flask.pocoo.org",
-##        views=16)
-##
-##    my_cat = add_cat("David Froehlingsdorf", 200, 100)
-##
-##    add_page(cat=my_cat,
-##        title="My Pythonanywhere page",
-##        url="https://www.pythonanywhere.com/user/2079884FDavid/",
-##        views=100)
-##
-##    add_page(cat=my_cat,
-##        title="My Github page",
-##        url="https://github.com/2079884FDavid/",
-##        views=100)
-##
-##    # Print out what we have added to the user.
-##    for c in Category.objects.all():
-##        for p in Page.objects.filter(category=c):
-##            print "- {0} - {1}".format(str(c), str(p))
-
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
-    return p
-
-def add_cat(name, views, likes):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
-    c.views = views
-    c.likes = likes
-    return c
 
 def add_user(name, email, password):
     u = User(username=name, email=email, password=password)
